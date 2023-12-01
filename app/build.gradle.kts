50fc5a611c104f1f77069d1f9a7800b0bafcd81f
lvminh97 <lvminh97@gmail.com>
2023-12-01 13:32:25 +0700

    Configure for building successful
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.kumail.scientificcalculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kumail.scientificcalculator"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
